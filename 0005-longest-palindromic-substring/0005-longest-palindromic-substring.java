53aaa9f626579e5f3e7922508f37ba5bf76d109f
Shristi Gupta <68809622+shristigupta1408@users.noreply.github.com>
2026-07-11 11:00:34 +1000

    Time: 14 ms (90.65%), Space: 43.7 MB (51.4%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -4,18 +4,6 @@ class Solution {
     String givenString;
 
     public String longestPalindrome(String s) {
-        // Would need a 2D DP array to track indices 
-        // if dp[i][j] = true, then s[i..j] is a palindrome
-        // Longest - where i - j or j - i is the maximum
-        // Odd numbered - when the left and right portions from the centre
-        // are palindromes too. 
-        // Not matching - 0 + find the palindrome of the rest of the string
-        // 2 options - move 1 pointer to the right, move the other to the left
-        // Palindrome - when the pointers meet, or they are adjacent, or pointers reach the end
-        // How do the pointers cross each other?
-        // Have a current pointer - and look at adjacent alphabets and keep moving those pointers
-        // But then how do you deal with even length palindromes?
-
         int leftPointer;
         int rightPointer;
         givenString = s;
@@ -41,13 +29,13 @@ class Solution {
         while (leftPointer >= 0 && rightPointer < givenString.length() 
                 && givenString.charAt(leftPointer) == givenString.charAt(rightPointer)) {
 
-                    if (rightPointer - leftPointer + 1 > lengthLongestPalindrome) {
-                        lengthLongestPalindrome = rightPointer - leftPointer + 1;
-                        startingIndex = leftPointer;
-                    }
-
-                leftPointer--;
-                rightPointer++;
+            if (rightPointer - leftPointer + 1 > lengthLongestPalindrome) {
+                lengthLongestPalindrome = rightPointer - leftPointer + 1;
+                startingIndex = leftPointer;
             }
+
+            leftPointer--;
+            rightPointer++;
+        }
     }
 }
