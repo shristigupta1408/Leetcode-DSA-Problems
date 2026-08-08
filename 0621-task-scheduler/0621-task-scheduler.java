5c05dc0e067f114f6b4ad439d6516857a995b834
Shristi Gupta <68809622+shristigupta1408@users.noreply.github.com>
2026-08-08 12:44:07 +1000

    Time: 4 ms (74.04%), Space: 48 MB (78.73%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,40 +1,59 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         // Max Heap Approach -
+        // int[] taskFreq = new int[26];
+
+        // for (char ch : tasks) {
+        //     taskFreq[ch - 'A'] += 1;
+        // }
+
+        // PriorityQueue<Integer> counts = new PriorityQueue<>(Collections.reverseOrder());
+        // Queue<int[]> remainingTasks = new LinkedList<>();
+
+        // for (int i = 0; i < taskFreq.length; i++) {
+        //     if (taskFreq[i] > 0) {
+        //         counts.add(taskFreq[i]);
+        //     }
+        // }
+
+        // int time = 0;
+
+        // while (!counts.isEmpty() || !remainingTasks.isEmpty()) {
+        //     time++;
+
+        //     if (counts.isEmpty()) {
+        //         time = remainingTasks.peek()[1];
+        //     } else {
+        //         int count = counts.poll() - 1;
+        //         if (count > 0) {
+        //             remainingTasks.add(new int[]{count, time + n});
+        //         }
+        //     }
+
+        //     if (!remainingTasks.isEmpty() && time == remainingTasks.peek()[1]) {
+        //         counts.add(remainingTasks.poll()[0]);
+        //     }
+        // } 
+
+        // return time;
+
+        // Greedy Approach -
+
         int[] taskFreq = new int[26];
 
         for (char ch : tasks) {
             taskFreq[ch - 'A'] += 1;
         }
 
-        PriorityQueue<Integer> counts = new PriorityQueue<>(Collections.reverseOrder());
-        Queue<int[]> remainingTasks = new LinkedList<>();
+        Arrays.sort(taskFreq);
+        int maxFreq = taskFreq[25];
 
-        for (int i = 0; i < taskFreq.length; i++) {
-            if (taskFreq[i] > 0) {
-                counts.add(taskFreq[i]);
-            }
+        int idleSlots = (maxFreq - 1) * n;
+
+        for (int i = 24; i >= 0; i--) {
+            idleSlots -= Math.min(maxFreq - 1, taskFreq[i]);
         }
 
-        int time = 0;
-
-        while (!counts.isEmpty() || !remainingTasks.isEmpty()) {
-            time++;
-
-            if (counts.isEmpty()) {
-                time = remainingTasks.peek()[1];
-            } else {
-                int count = counts.poll() - 1;
-                if (count > 0) {
-                    remainingTasks.add(new int[]{count, time + n});
-                }
-            }
-
-            if (!remainingTasks.isEmpty() && time == remainingTasks.peek()[1]) {
-                counts.add(remainingTasks.poll()[0]);
-            }
-        } 
-
-        return time;
+        return Math.max(0, idleSlots) + tasks.length;
     }
 }
